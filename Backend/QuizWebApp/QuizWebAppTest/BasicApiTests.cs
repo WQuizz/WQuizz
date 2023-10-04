c78fe22cdb1b367dcd72c747adb0555bd57cb41d
McGerasz <mcgerasz@gmail.com>
2023-10-04 22:05:09 +0200

    feat: test to see if workflow works

--- Backend/QuizWebApp/QuizWebAppTest/BasicApiTests.cs
+++ Backend/QuizWebApp/QuizWebAppTest/BasicApiTests.cs
@@ -1,15 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using QuizWebApp.Controllers;
+using QuizWebApp.DatabaseServices;
+using QuizWebApp.DatabaseServices.Repositories;
+
 namespace QuizWebAppTest;
 
 public class Tests
 {
+    private QuizController _quizController;
+    private IQuizRepository _quizRepository;
+    private WQuizzDBContext _quizzDbContext;
     [SetUp]
     public void Setup()
     {
+        _quizzDbContext = new WQuizzDBContext();
+        _quizRepository = new QuizRepository(_quizzDbContext);
+        _quizController = new QuizController(_quizRepository);
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var result = _quizController.GetRandomApprovedQuiz();
+        Assert.That(result, !Is.Null);
     }
 }
