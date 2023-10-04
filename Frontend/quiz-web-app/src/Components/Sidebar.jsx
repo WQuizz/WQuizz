2e954c34ed53744b8c84f54e09a783dcdccf0c69
fejesmartin <fejes.martin00@gmail.com>
2023-10-04 11:27:24 +0200

    feature: adds Siderbar component and css file

---
+++ Frontend/quiz-web-app/src/Components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import "../Styles/sidebar.css";
+import sidebarButton from '../Images/sidebar-button.png';
+import homeIcon from '../Images/home-icon.png';
+import categoriesIcon from '../Images/categories-icon.png';
+import leaderboardsIcon from '../Images/leaderboards-icon.png';
+
+function Sidebar() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+      <Navbar expand="lg">
+        <button onClick={toggleSidebar} className='toggle-button'><img src={sidebarButton}></img></button>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="flex-column sidebar-items">
+            <Nav.Item>
+              <Nav.Link href="#">{isOpen ?'Home': <img src={homeIcon}></img>}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#">{isOpen ?'Categories': <img src={categoriesIcon}></img>}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#">{isOpen ?'Leaderboards': <img src={leaderboardsIcon}></img>}</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#">Sign out</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Sidebar;
