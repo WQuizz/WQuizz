f13d358835e22f9dcf1a16902fd773766ba13ae1
fejesmartin <fejes.martin00@gmail.com>
2023-10-04 14:35:11 +0200

    feature: adds random recommendation div
    Co-authored-by: Krisztofer Burka <burkakrisztofer@hotmail.com>

--- Frontend/quiz-web-app/src/Pages/DummyPage.jsx
+++ Frontend/quiz-web-app/src/Pages/DummyPage.jsx
@@ -2,12 +2,14 @@ import DummyText from "../Components/DummyText"
 import "../Styles/homepage.css";
 import NavigationBar from "../Components/NavigationBar.jsx"
 import Sidebar from "../Components/Sidebar";
+import RecommendationElement from "../Components/RecommendationElement";
 
 export default function DummyPage(){
 
 return(
        
     <div className="Homepage">
+        <RecommendationElement />
         <DummyText/>
         <Sidebar />
     </div>
