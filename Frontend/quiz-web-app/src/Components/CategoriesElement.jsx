f80dc48a710bd8131d2035b060d3624ca3e64b9f
fejesmartin <fejes.martin00@gmail.com>
2023-10-05 15:53:55 +0200

    feature: adds category page and design
    Co-authored-by: Krisztofer Burka <burkakrisztofer@hotmail.com>
    Co-authored-by: Gergő Szőke <McGerasz@users.noreply.github.com>

--- Frontend/quiz-web-app/src/Components/CategoriesElement.jsx
+++ Frontend/quiz-web-app/src/Components/CategoriesElement.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
 import categoryObjects from "../Files/CategoryObjects.js"
+import "../Styles/categorypage.css";
 
 export default function CategoriesElement() {
     const [categories, setCategories] = useState([])
 
     useEffect(() => {
-        fetch("http://localhost:9000/api/Category")
+        fetch("http://localhost:5015/api/Category")
             .then(res => res.json())
             .then(category => setCategories(category))
             .catch(error => console.log(error))
@@ -13,22 +14,36 @@ export default function CategoriesElement() {
 
     return (
         <>
-            {categories && categories.map((c, index) => {
-                const matchingCategory = categoryObjects.find(co => c.toLowerCase() === co.name.toLowerCase());
-                if (matchingCategory) {
-                    return (
-                        <div key={index}>
-                            {c}
-                            <img src={matchingCategory.url} alt={c} />
-                        </div>
-                    );
-                } else {
-                    return null; // Or handle the case where no matching category is found
-                }
-            })}
+        <h1 className="category-title">Categories</h1>
+        <div className="category-page">
+            <div className="flex-container">
+                {categories &&
+                    categories.map((c, index) => {
+                        const matchingCategory = categoryObjects.find(
+                            (co) => c.toLowerCase() === co.name.toLowerCase()
+                        );
+                        if (matchingCategory) {
+                            return (<>
+                                    
+                                    <div className="category-content-wrapper">
+                                    <img
+                                        className="category-images"
+                                        src={matchingCategory.url}
+                                        alt={c}
+                                    ></img><div className="text-section">{c}</div>
+                                    </div>
+                                    
+                            </>);
+                        } else {
+                            return null; 
+                        }
+                    })}
+            </div>
+        </div>
         </>
-    )
+    );
 }
+
 
 
 
