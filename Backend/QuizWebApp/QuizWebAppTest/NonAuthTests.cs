d51846a0349ce8e78844b984539f6218a2c3970d
McGerasz <mcgerasz@gmail.com>
2023-10-16 13:21:39 +0200

    chore: init

---
+++ Backend/QuizWebApp/QuizWebAppTest/NonAuthTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using QuizWebApp.DatabaseServices;
+using QuizWebApp.DatabaseServices.Repositories;
+using QuizWebAppTest.TestContexts;
+
+namespace QuizWebAppTest;
+
+public class Tests
+{
+    
+}
