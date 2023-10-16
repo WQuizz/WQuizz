41d177e3502ca6f5bfadc31723b5c16b2b0937cb
McGerasz <mcgerasz@gmail.com>
2023-10-16 13:23:26 +0200

    test: add test for seeding quizrepo

--- Backend/QuizWebApp/QuizWebAppTest/NonAuthTests.cs
+++ Backend/QuizWebApp/QuizWebAppTest/NonAuthTests.cs
@@ -8,5 +8,22 @@ namespace QuizWebAppTest;
 
 public class Tests
 {
-    
+    private static DbContextOptions<WQuizzDBContext> _dbContextOptions;
+    private TestWQuizzDBContext _dbContext;
+    private IQuizRepository _quizRepository;
+    [SetUp]
+    public void Setup()
+    {
+        _dbContextOptions = new DbContextOptionsBuilder<WQuizzDBContext>().UseInMemoryDatabase(databaseName: "WQuizzDB")
+            .Options;
+        _dbContext = new TestWQuizzDBContext();
+        _quizRepository = new QuizRepository(_dbContext);
+    }
+
+    [Test]
+    public void TheQuizzesAreSeedIntoDatabase()
+    {
+        var seededData = _quizRepository.GetAll();
+        Assert.That(seededData.Count(), Is.EqualTo(4));
+    }
 }
