d32e7bf76f69872444106923139d37b2bd32de5f
chris924 <burkakrisztofer@hotmail.com>
2023-10-17 17:14:10 +0200

    feat: questins and answers displayed

--- Frontend/quiz-web-app/src/Components/QuizElement.jsx
+++ Frontend/quiz-web-app/src/Components/QuizElement.jsx
@@ -6,54 +6,132 @@ import quizImage from "../Images/quiz-time.png";
 
 
 export default function QuizElement(props) {
-  const [quizResult, setQuizResult] = useState("");
+  const [quizResult, setQuizResult] = useState([]);
+  const [quizQuestions, setQuizQuestions] = useState([]);
+  const [quizAnswers, setQuizAnswers] = useState([]);
+
+  const [currentQuestion, setCurrentQuestion] = useState("");
+
+  const [questionNumberTracker, setQuestionNumberTracker] = useState(1);
+
+  const [endofQuiz, setEndOfQuiz] = useState(false);
+
+  async function FetchAnswers()
+  {
+    try{
+      const response = await fetch(`http://localhost:9000/api/Answer/GetByQuestionId/${quizQuestions[0].id}`);
+      const data = await response.json();
+      if(response.ok)
+      {
+        setQuizAnswers(data);
+        console.log(data);
+      }
+     
+
+    }catch(err)
+    {
+      console.error(err);
+    }
+  }
 
 
+async function FetchQuestions()
+{
+  try{
+    console.log(quizResult.id);
+    const response = await fetch(`http://localhost:9000/api/Question/GetByQuizId/${quizResult.id}`)
+    const data =  await response.json();
+    if(response.ok)
+    {
+      setQuizQuestions(data);
+      setCurrentQuestion(data[0].questionContent)
+      console.log(data);
+    }
+   
+
+  }catch(err){
+    console.error(err);
+  }
+
+}
 
 
   async function FetchSearchInput() {
     try {
+      console.log(props.quizName);
       const response = await fetch(
         `http://localhost:9000/api/Quiz/GetQuizByName?name=${props.quizName}`
       );
       const data = await response.json();
-      setQuizResult(data);
-      console.log(data);
+      if(response.ok)
+      {
+        console.log(data);
+        setQuizResult(data);
+        console.log(quizResult);
+      }
+      
     } catch (err) {
       console.error(err);
     }
   }
 
+
   useEffect(() => {
     FetchSearchInput();
   }, []);
 
-useEffect(()=>{
-  console.log(quizResult);
-},[quizResult])
+  useEffect(() => {
+    FetchQuestions();
+  }, [quizResult])
+
+  useEffect(() =>{
+    FetchAnswers();
+  }, [quizQuestions])
+
+  const handleClick = () => {
+
+
+    if(quizQuestions.length == questionNumberTracker)
+    {
+      setEndOfQuiz(true);
+      console.log("END OF QUIZ");
+    }
+    else{
+
+      setQuestionNumberTracker((prevState) => {
+        return prevState + 1;
+      });
+  
+      setCurrentQuestion(quizQuestions[questionNumberTracker].questionContent)
+    }
+
+   
+  }
+
+
+
 
   return (
     <>
-      {quizResult && (
-        <>
+      {quizQuestions && quizResult && quizAnswers && quizQuestions.length > 0 && quizAnswers.length > 0 && endofQuiz == false ? (
         <div className="maincontainer animated-background">
           <div className="quiz-time-image-container"><img className="quiz-time-image" src={quizImage}></img></div>
           <div className="quiz-container">
-          <div className="quiz-question">{quizResult.quizName}</div>
+          <div className="quiz-title">{quizResult.quizName}</div>
+          <div className="quiz-question">{currentQuestion}</div>
           <div className="quiz-answers">
             <div className="answer-row first-row"> 
-            <button className="answer">Answer 1</button>
-            <button className="answer">Answer 2</button>
+            <button className="answer" onClick={(e) => handleClick()}>{quizAnswers[0].answerContent}</button>
+            <button className="answer" onClick={(e) => handleClick()}>{quizAnswers[1].answerContent}</button>
             </div>
           <div className="answer-row second-row"> 
-          <button className="answer">Answer 3</button>
-            <button className="answer">Answer 4</button>
+          <button className="answer" onClick={(e) => handleClick()}>{quizAnswers[2].answerContent}</button>
+            <button className="answer" onClick={(e) => handleClick()}>{quizAnswers[3].answerContent}</button>
           </div>
           </div>
           </div>
           </div>
-        </>
-      )}
+      ) : (<div>END OF QUIZ</div>)}
     </>
   );
 }
