904710b9e4c333ece16dab60fbf48a5ca0c3fe40
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-10-17 16:08:37 +0200

    feat: Implements api call to Auth/Register and creates new user

--- Frontend/quiz-web-app/src/Pages/RegisterPage.jsx
+++ Frontend/quiz-web-app/src/Pages/RegisterPage.jsx
@@ -1,10 +1,69 @@
+import { useState, useEffect } from "react"
 import RegisterElement from "../Components/RegisterElement";
+import SuccessfullElement from "../Components/SuccessfullElement";
+import { useNavigate } from 'react-router-dom';
 import "../Styles/register.css";
 
 export default function RegisterPage(){
+    const [email, setEmail] = useState(null);
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [response, setResponse] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        const data = {
+            email: email,
+            username: username,
+            password: password,
+        }
+
+        fetch('http://localhost:9000/Auth/Register', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(r => {setResponse(r)})
+            .catch(err=>console.error(err))
+    };
+
+    useEffect(()=>{
+        console.log(response);
+        if (response && 'DuplicateEmail'||'DuplicateUserName' in response) {
+            //We can rewrite this later to specify email or username if we want
+            setErrorMessage('Username or e-mail is already in use'); 
+        }
+        if(response && 'email' in response){
+            setShowSuccessMessage(true);
+            setTimeout(() => {
+                setShowSuccessMessage(false);
+                navigate("/");
+            }, 3000);
+        }
+    },[response])
+
     return(
             <div className="register-page-element-container">
-                <RegisterElement></RegisterElement>
+                {showSuccessMessage && (
+                <>
+                    <SuccessfullElement message={"Successfully registered"}/>
+                    <div className="register-successoverlay" />
+                </>
+                )}
+                <RegisterElement 
+                    setEmail={setEmail} 
+                    setUsername={setUsername} 
+                    setPassword ={setPassword} 
+                    handleSubmit={handleSubmit}
+                    errorMessage={errorMessage}
+                    ></RegisterElement>
             </div>
     
         )
