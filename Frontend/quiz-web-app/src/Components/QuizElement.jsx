758d07839f444b7e8fe0e642ec8c40fdc4cfbca7
fejesmartin <fejes.martin00@gmail.com>
2023-10-17 14:09:11 +0200

    feature: adds background animation and quiz site
    Co-authored-by: Krisztofer Burka <burkakrisztofer@hotmail.com>

--- Frontend/quiz-web-app/src/Components/QuizElement.jsx
+++ Frontend/quiz-web-app/src/Components/QuizElement.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import "../Styles/quizelement.css";
+import "../Styles/animated-background.css";
+import quizImage from "../Images/quiz-time.png";
+
 
 export default function QuizElement(props) {
   const [quizResult, setQuizResult] = useState("");
+
+
+
 
   async function FetchSearchInput() {
     try {
@@ -21,14 +28,30 @@ export default function QuizElement(props) {
     FetchSearchInput();
   }, []);
 
+useEffect(()=>{
+  console.log(quizResult);
+},[quizResult])
+
   return (
     <>
       {quizResult && (
         <>
-          <h1 style={{ fontSize: 1000, zIndex: 1000000, color: "white" }}>
-            {props.quizName}
-          </h1>
-          <h1>{quizResult.quizName}</h1>
+        <div className="maincontainer animated-background">
+          <div className="quiz-time-image-container"><img className="quiz-time-image" src={quizImage}></img></div>
+          <div className="quiz-container">
+          <div className="quiz-question">{quizResult.quizName}</div>
+          <div className="quiz-answers">
+            <div className="answer-row first-row"> 
+            <button className="answer">Answer 1</button>
+            <button className="answer">Answer 2</button>
+            </div>
+          <div className="answer-row second-row"> 
+          <button className="answer">Answer 3</button>
+            <button className="answer">Answer 4</button>
+          </div>
+          </div>
+          </div>
+          </div>
         </>
       )}
     </>
