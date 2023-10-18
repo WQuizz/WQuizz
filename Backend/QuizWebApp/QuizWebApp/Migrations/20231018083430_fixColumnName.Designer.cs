a52dc583eff8224ec18b8ac1d5388ba94605c490
McGerasz <mcgerasz@gmail.com>
2023-10-18 10:45:03 +0200

    feat: add new migrations

--- Backend/QuizWebApp/QuizWebApp/Migrations/20231018083430_fixColumnName.Designer.cs
+++ Backend/QuizWebApp/QuizWebApp/Migrations/20231018083430_fixColumnName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizWebApp.DatabaseServices;
 
@@ -11,9 +12,11 @@ using QuizWebApp.DatabaseServices;
 namespace QuizWebApp.Migrations
 {
     [DbContext(typeof(WQuizzDBContext))]
-    partial class WQuizzDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231018083430_fixColumnName")]
+    partial class fixColumnName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
