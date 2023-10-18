7e3178fceda988780431c12c72bef42cc32aaf8a
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2023-10-18 23:09:37 +0200

    feat: Fetch and display profile picture from profile

---
+++ Frontend/quiz-web-app/src/Components/DisplayProfileImageElement.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import "../Styles/profilepage.css";
+
+function DisplayProfileImageElement({profilePicture}) {
+  const dataUrl = `data:image/jpeg;base64,${profilePicture}`;
+
+const defaultImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg'; // Replace with your default image URL
+
+  return (
+    <div className='profile-picture-container'>
+        {profilePicture ? (<img src={dataUrl} alt="Uploaded" className='profile-picture'/>) : (<img src={defaultImageUrl} alt="Default" className='profile-picture'/>)}
+    </div>
+  );
+}
+
+export default DisplayProfileImageElement;
